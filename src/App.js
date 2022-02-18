d0c8420caf0f562692d2121550cdaf2973afdd4b
hunterfairy99@icloud.com <phucan332000@gmail.com>
2022-02-18 09:09:10 +0700

    two-way biding react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// const gifts = ["con mèo", "con chó", "con vẹt"];
 
 function App() {
+  // const [gift, setGift] = useState();
+
+  // const randomGift = () => {
+  //   const index = Math.floor(Math.random() * gifts.length);
+  //   setGift(gifts[index]);
+  // };
+
+  //two-way binding
+
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+
+  // const handleSubmit = () => {
+  //   console.log({
+  //     name,
+  //     email,
+  //   });
+  // };
+
+  //radio
+  const courses = [
+    {
+      id: 1,
+      name: "HTML, CSS",
+    },
+    {
+      id: 2,
+      name: "Javascript",
+    },
+    {
+      id: 3,
+      name: "ReactJS",
+    },
+  ];
+
+  //const [checked, setChecked] = useState(2);
+
+  const [checked, setChecked] = useState([]);
+
+  console.log(checked);
+
+  const handleSubmit = () => {
+    //call API
+    console.log({ id: checked });
+  };
+
+  const handleCheck = (id) => {
+    setChecked((prev) => {
+      const isChecked = checked.includes(id);
+      if (isChecked) {
+        //uncheck
+        return checked.filter((item) => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 32 }}>
+      {/* <h1>{gift || "Chưa có phần thưởng"}</h1>
+      <button onClick={randomGift}>Lấy thưởng</button> */}
+
+      {/* <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input value={email} onChange={(e) => setEmail(e.target.value)} /> */}
+
+      {/*      
+      // radio
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type="radio"
+            onChange={() => setChecked(course.id)}
+            checked={checked === course.id}
+          />
+          {course.name}
+        </div>
+      ))} */}
+
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type="checkbox"
+            checked={checked.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
